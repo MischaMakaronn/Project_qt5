4eb7818e2f6a84dbc0ee5ab4cfba7387bf474800
COSMICCOLOR <dzikipm@gmail.com>
2023-06-11 21:10:50 +0300

    1st commit DB was created

---
+++ DBwarehouse.py
@@ -0,0 +1,74 @@
+import sqlite3 as sl
+
+"""
+SELECT ('столбцы или * для выбора всех столбцов; обязательно')
+FROM ('таблица; обязательно')
+WHERE ('условие/фильтрация, например, city = 'Moscow'; необязательно')
+GROUP BY ('столбец, по которому хотим сгруппировать данные; необязательно')
+HAVING ('условие/фильтрация на уровне сгруппированных данных; необязательно')
+ORDER BY ('столбец, по которому хотим отсортировать вывод; необязательно')
+"""
+
+con = sl.connect('warehouse.db')
+
+with con:
+    con.execute("""
+        CREATE TABLE IF NOT EXISTS Clients (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
+        name TEXT, 
+        phone_number TEXT, 
+        address TEXT,
+        UNIQUE (phone_number)
+        );
+    """)
+
+    con.execute("""
+        CREATE TABLE IF NOT EXISTS Category (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            description TEXT
+        );
+    """)
+
+    con.execute("""
+        CREATE TABLE IF NOT EXISTS Goods (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            code TEXT,
+            name TEXT,
+            description TEXT,
+            photo TEXT,
+            price FLOAT,
+            weight FLOAT,
+            unit TEXT,
+            count INTEGER,
+            expiration_date DATETIME,
+            sold  INTEGER,
+            write_off INTEGER,
+            category_id INTEGER,
+            stock_id INTEGER,
+            FOREIGN KEY (category_id) REFERENCES Category (id),
+            FOREIGN KEY (stock_id) REFERENCES Stock (id)
+        );
+    """)
+
+    con.execute("""
+            CREATE TABLE IF NOT EXISTS Orders (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                client_id INTEGER,
+                good_id TEXT,
+                total_price FLOAT,
+                date DATETIME,
+                FOREIGN KEY (client_id) REFERENCES Clients (id),
+                FOREIGN KEY (good_id) REFERENCES Goods (id)
+            );
+        """)
+
+    con.execute("""
+            CREATE TABLE IF NOT EXISTS Stock (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                address TEXT,
+                geo_text TEXT,
+                geo_coordinates TEXT
+            )
+        """)
