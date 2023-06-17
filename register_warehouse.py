9d87a796a43a4fe6b794a8692da9a2f559a27f52
MischaMakaronn <102899389+MischaMakaronn@users.noreply.github.com>
2023-06-17 22:48:55 +0300

    bd was filld ed

--- register_warehouse.py
+++ register_warehouse.py
@@ -2,11 +2,15 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QPushButton, QDialog, QPushButton
 from functools import partial
 import sqlite3
+from geopy.geocoders import Nominatim
 # import datetime as DT
 # print(DT.datetime.now())
 # print(str(DT.datetime.now())[:10])
 import os
 import docxtpl
+
+global geolocator
+geolocator = Nominatim(user_agent="AIzaSyDDMUpXVY_MU1Z0m0ZTBLzZdI9BkYboBpA")
 
 
 class Ui_Dialog(object):
@@ -23,12 +27,14 @@ class Ui_Dialog(object):
         self.lineEdit.setGeometry(QtCore.QRect(100, 70, 113, 22)) # 1 координаты, 2 размеры
         self.lineEdit.setStyleSheet("border-radius:20px;\n" "border:2px solid #ffd;")
         self.lineEdit.setObjectName("lineEdit")
+
+
         self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(70, 30, 171, 31))
+        self.label.setGeometry(QtCore.QRect(100, 30, 171, 31))
         self.label.setStyleSheet("color:#ffd;\n" "font: 12pt \"Simplex_IV50\";")
         self.label.setObjectName("label")
         self.label_3 = QtWidgets.QLabel(Dialog)
-        self.label_3.setGeometry(QtCore.QRect(300, 30, 191, 31))
+        self.label_3.setGeometry(QtCore.QRect(330, 30, 191, 31))
         self.label_3.setStyleSheet("color:#ffd;\n" "font: 12pt \"Simplex_IV50\";")
         self.label_3.setObjectName("label_3")
         self.lineEdit_2 = QtWidgets.QLineEdit(Dialog)
@@ -39,6 +45,9 @@ class Ui_Dialog(object):
         self.pushButton_3.setGeometry(QtCore.QRect(120, 340, 101, 41))
         self.pushButton_3.setStyleSheet("color: #fff;\n" "\n" "background:rgb(120, 192, 255)")
         self.pushButton_3.setObjectName("pushButton_3")
+
+
+
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
@@ -55,19 +64,26 @@ class Ui_Dialog(object):
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
         self.accept_pushButton.setText(_translate("Dialog", "Подверить "))
         self.label.setText(_translate("Dialog", "Название"))
+
         self.label_3.setText(_translate("Dialog", "Введите адрес"))
+
         self.pushButton_3.setText(_translate("Dialog", "Отменить"))
         self.pushButton_3.clicked.connect(Dialog.close)
         self.accept_pushButton.clicked.connect(partial(self.save_stock))
 
     def save_stock(self):
+        location = geolocator.geocode(self.lineEdit_2.text())
+        print(location.address)
+        print((location.latitude, location.longitude))
+
         conn = sqlite3.connect('warehouse.db')
         with conn:
-            conn.execute("INSERT OR IGNORE INTO Stock (name, address) values(?, ?)",
-                         (self.lineEdit.text(), self.lineEdit_2.text()))
+            conn.execute("INSERT OR IGNORE INTO Stock (name, address, geo_text, geo_coordinates) values(?, ?, ?, ?)",
+                         (self.lineEdit.text(), self.lineEdit_2.text(), location.address, f'{location.latitude}, {location.longitude}' ))
         conn.commit()
         self.lineEdit.clear()
         self.lineEdit_2.clear()
+
 
 
 
